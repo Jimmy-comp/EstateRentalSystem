82c3db962b36017c16369e717aba1adf4f4d2cdb
Jimmy-comp <jimmycyc.0420@gmail.com>
2025-10-18 00:45:11 -0600

    Update logout navigation problem

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,64 +1,73 @@
 import { type FC } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import homeLogo from "../assets/home_logo.png"
 import { useUserContext } from "../context/UserContext";
 
 const Navbar: FC = () => {
   const {user, logout} = useUserContext();
-  const navigate = useNavigate();
 
   // Simulate login/logout
   const handleLogout = () => {
     logout();
-    
-    navigate("/");
+    alert("You are logged out.")
+    window.location.replace("/");
   };
 
   return (
-    <nav className="w-full bg-gray-300 text-white shadow-md">
+    <nav className="w-full bg-gray-300 shadow-md text-blue-600">
       <div className="max-w-8xl mx-auto px-4 py-3 flex items-center justify-between">
         {/* Left Section */}
-        <div className="flex left-0 space-x-6">
+        <div className="flex items-center space-x-5">
+            {/* Home / Logo */}
             <Link to="/">
-                <img src={homeLogo} alt="Home Logo" className="w-10 h-10 mix-blend-multiply" />
+                <img src={homeLogo} alt="Home Logo" className="w-10 h-10" />
             </Link>
-            <a href="/" className="flex items-center space-x-2 hover:text-gray-200">
-                <span className="font-semibold text-lg">Home</span>
-            </a>
 
-            <a href="/search" className="flex items-center space-x-2 hover:text-gray-200">
-                <span>Search</span>
-            </a>
+            {/* Link */}
+            <ul className="flex items-center space-x-5 text-lg">
+                <li>
+                    <Link to="/" className="font-semibold text-xl hover:text-gray-900">Home</Link>
+                </li>
+                <li>
+                    <Link to="/search" className="hover:text-gray-900">Search</Link>
+                </li>
 
-          {/* Role-based navigation */}
-          {user?.role === "admin" && (
-            <>
-                <a href="/agent" className="flex items-center space-x-2 hover:text-gray-200">Create</a>
-                <a href="/admin" className="flex items-center space-x-2 hover:text-gray-200">Admin</a>
-            </>
-          )}
-          {user?.role === "landlord" && (
-            <a href="/agent" className="flex items-center space-x-2 hover:text-gray-200">Create</a>
-          )}
-          {user?.role === "tenant" && (
-            <a href="/tenant" className="flex items-center space-x-2 hover:text-gray-200">My Rentals</a>
-          )}
+                {/* Role-based navigation */}
+                {user?.role === "admin" && (
+                    <>
+                        <li>
+                            <Link to="/create" className="hover:text-gray-900">Create</Link>
+                            <Link to="/admin" className="hover:text-gray-900">Admin</Link>
+                        </li>
+                    </>
+                )}
+                {user?.role === "landlord" && (
+                    <li>
+                        <Link to="/create" className="hover:text-gray-900">Create</Link>
+                    </li>
+                )}
+                {user?.role === "tenant" && (
+                    <li>
+                        <Link to="/MyRental" className="hover:text-gray-900">MyRental</Link>
+                    </li>
+                )}
+            </ul>
         </div>
 
         {/* Right Section */}
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-4 text-lg">
           {user ? (
             <>
-              <span className="font-medium">👋 {user.username}</span>
+              <span className="font-medium">Welcome, {user.username}</span>
               <button
                 onClick={handleLogout}
-                className="bg-white text-blue-600 px-4 py-1 rounded hover:bg-gray-100"
+                className="bg-white px-4 py-1 rounded hover:bg-gray-100"
               >
                 Logout
               </button>
             </>
           ) : (
-            <Link to="/login" className="bg-white text-blue-600 px-4 py-1 rounded hover:bg-gray-100">
+            <Link to="/login" className="bg-white px-4 py-1 rounded hover:bg-gray-100">
                 Login
             </Link>
           )}
