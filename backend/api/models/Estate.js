0433b36eb7473fb2ebdc9835f879d6927e5f0f39
Jimmy-comp <jimmycyc.0420@gmail.com>
2025-10-17 21:40:53 -0600

    Update data model for new update

---
+++ backend/api/models/Estate.js
@@ -0,0 +1,64 @@
+/**
+ * Estate.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    title: {
+      type: "string"
+    },
+
+    estatename: {
+      type: "string"
+    },
+
+    district: {
+      type: "string"
+    },
+
+    area: {
+      type: "number"
+    },
+
+    rent: {
+      type: "number"
+    },
+
+    url: {
+      type: "string"
+    },
+
+    roomnum: {
+      type: "number"
+    },
+ 
+    tenants: {
+      type: "number"
+    },
+
+    highlighted: {
+      type: "boolean",
+      defaultsTo: false
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    viewFrom: {
+      collection: 'User',
+      via: 'supervises'
+    },
+  },
+};
+
---
+++ backend/api/models/User.js
@@ -0,0 +1,50 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    username: {
+      type: 'string',
+      unique: true,
+      required: true
+    },
+
+    password: {
+      type: 'string',
+      unique: true,
+      required: true
+    },
+
+    role: {
+      type: 'string',
+      isIn: ['admin', 'landlord', 'tenant'],
+      defaultsTo: 'visitor'
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    supervises: {
+      collection: 'Estate',
+      via: 'viewFrom'
+    },
+  },
+
+  customToJSON: function () {
+    // Return a shallow copy of this record with the password removed.
+    return _.omit(this, ['password'])
+  },
+};
+
